7f3650c543d6c9fe069c4249291c156de7173ac8
Vladimir Vorozbit <138519316+Vladimero@users.noreply.github.com>
2023-10-19 12:40:40 +0200

    Create main and blank page structure

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+// This is the main "platform" or "Community" page, where the user takes actions after he registered and signed in
+
+export default function DashboardPage() {
+  return <div>Enter</div>;
+}
---
+++ app/result/page.tsx
@@ -0,0 +1,5 @@
+// Something like a "Thank you page" after the user submits an asset --> should throw an offered action with a confirmation message
+
+export default function ResultPage() {
+  return <div>Enter</div>;
+}
