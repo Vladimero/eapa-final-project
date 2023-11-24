2a681f8c783bb936796f486cf6f2ba603de6ef75
Vladimir Vorozbit <138519316+Vladimero@users.noreply.github.com>
2023-11-24 13:19:01 +0100

    Add favicon

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,24 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getValidSessionByToken } from '../../../database/sessions';
 import LoginForm from './LoginForm';
 
 type Props = { searchParams: { returnTo?: string | string[] } };
 
-export default function LoginPage({ searchParams }: Props) {
+export default async function LoginPage({ searchParams }: Props) {
+  // 1. Check if the cookie with session token exists
+  const sessionTokenCookie = cookies().get('sessionToken');
+
+  // 2. Check if the session token is valid
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
+
+  // 3. If session token is valid redirect to homepage
+  if (session) redirect('/');
+
+  // 4. If session token is invalid redirect to the login form
+
   return (
     <div>
       <LoginForm returnTo={searchParams.returnTo} />
